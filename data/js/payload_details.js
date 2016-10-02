a120ea441a4f16026451f8e1a4f7737fb9b49cf7
Tim Klapdor <tim.klapdor@gmail.com>
2016-10-02 11:16:58 +1100

    Added 178 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11095,
-	"created_at": "Sat Oct 01 00:16:55 +0000 2016",
+	"tweets": 11097,
+	"created_at": "Sun Oct 02 00:16:57 +0000 2016",
 	"lang": "en"
 }
---
+++ data/js/tweets/2016_10.js
@@ -0,0 +1,495 @@
+Grailbird.data.tweets_2016_10 = 
+[
+	{
+		"created_at": "Sat Oct 01 09:27:32 +0000 2016",
+		"id": 782149941971914800,
+		"id_str": "782149941971914753",
+		"text": "@dajbelshaw @mrkrndvs Love to hear some feedback to be honest.",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dajbelshaw",
+					"name": "Doug Belshaw",
+					"id": 764365,
+					"id_str": "764365",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "mrkrndvs",
+					"name": "Aaron Davis",
+					"id": 372768752,
+					"id_str": "372768752",
+					"indices": [
+						12,
+						21
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 781731391658664000,
+		"in_reply_to_status_id_str": "781731391658663936",
+		"in_reply_to_user_id": 764365,
+		"in_reply_to_user_id_str": "764365",
+		"in_reply_to_screen_name": "dajbelshaw",
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Wagga Wagga",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology.",
+			"url": "http://t.co/uC0c3me6KE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/uC0c3me6KE",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 680,
+			"friends_count": 175,
+			"listed_count": 95,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 6064,
+			"utc_offset": 39600,
+			"time_zone": "Sydney",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11116,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000014291852/792df4c6e7490b807e8405ea6be45833.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000014291852/792df4c6e7490b807e8405ea6be45833.jpeg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/723129079671615492/O_NQ9rKL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/723129079671615492/O_NQ9rKL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sat Oct 01 06:01:21 +0000 2016",
+		"id": 782098053410857000,
+		"id_str": "782098053410856964",
+		"text": "RT @JillFilipovic: Can we please not elect a man who sexualizes 12-year-old girls and watches sex tapes of his friends' kids? https://t.co/…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JillFilipovic",
+					"name": "Jill Filipovic",
+					"id": 16378093,
+					"id_str": "16378093",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Wagga Wagga",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology.",
+			"url": "http://t.co/uC0c3me6KE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/uC0c3me6KE",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 680,
+			"friends_count": 175,
+			"listed_count": 95,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 6064,
+			"utc_offset": 39600,
+			"time_zone": "Sydney",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11116,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000014291852/792df4c6e7490b807e8405ea6be45833.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000014291852/792df4c6e7490b807e8405ea6be45833.jpeg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/723129079671615492/O_NQ9rKL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/723129079671615492/O_NQ9rKL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Sep 30 17:00:25 +0000 2016",
+			"id": 781901526449127400,
+			"id_str": "781901526449127424",
+			"text": "Can we please not elect a man who sexualizes 12-year-old girls and watches sex tapes of his friends' kids? https://t.co/mejl1CUoIW",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mejl1CUoIW",
+						"expanded_url": "https://twitter.com/JessicaValenti/status/781886874436636672",
+						"display_url": "twitter.com/JessicaValenti…",
+						"indices": [
+							107,
+							130
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16378093,
+				"id_str": "16378093",
+				"name": "Jill Filipovic",
+				"screen_name": "JillFilipovic",
+				"location": "Nairobi | Brooklyn",
+				"description": "Writer. Lawyer. Eater. Author of the forthcoming The H-Spot: The Feminist Pursuit of Happiness.",
+				"url": "http://t.co/SXxCvWunAd",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/SXxCvWunAd",
+								"expanded_url": "http://www.jillfilipovic.com",
+								"display_url": "jillfilipovic.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 43364,
+				"friends_count": 3984,
+				"listed_count": 1604,
+				"created_at": "Sat Sep 20 14:36:18 +0000 2008",
+				"favourites_count": 15026,
+				"utc_offset": -14400,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 33312,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B89807",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/81542256/MadonnaAmyArbus.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/81542256/MadonnaAmyArbus.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/565966638181134336/i9LG__yZ_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/565966638181134336/i9LG__yZ_normal.jpeg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16378093/1363029764",
+				"profile_link_color": "0E0C54",
+				"profile_sidebar_border_color": "FFBF00",
+				"profile_sidebar_fill_color": "88DAE8",
+				"profile_text_color": "611961",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 781886874436636700,
+			"quoted_status_id_str": "781886874436636672",
+			"quoted_status": {
+				"created_at": "Fri Sep 30 16:02:12 +0000 2016",
+				"id": 781886874436636700,
+				"id_str": "781886874436636672",
+				"text": "How the fuck did I miss this? https://t.co/N6Ge5B0eIp https://t.co/rwaHCDpuf5",
+				"truncated": false,
+				"entities": {
+					"hashtags": [],
+					"symbols": [],
+					"user_mentions": [],
+					"urls": [
+						{
+							"url": "https://t.co/N6Ge5B0eIp",
+							"expanded_url": "http://www.thedailybeast.com/articles/2015/08/02/donald-trump-s-douchebaggiest-interview-ever-any-girl-you-have-i-can-take-from-you.html",
+							"display_url": "thedailybeast.com/articles/2015/…",
+							"indices": [
+								30,
+								53
+							]
+						}
+					],
+					"media": [
+						{
+							"id": 781886865909682200,
+							"id_str": "781886865909682177",
+							"indices": [
+								54,
+								77
+							],
+							"media_url": "http://pbs.twimg.com/media/CtnR9kEXEAEIFQI.jpg",
+							"media_url_https": "https://pbs.twimg.com/media/CtnR9kEXEAEIFQI.jpg",
+							"url": "https://t.co/rwaHCDpuf5",
+							"display_url": "pic.twitter.com/rwaHCDpuf5",
+							"expanded_url": "https://twitter.com/JessicaValenti/status/781886874436636672/photo/1",
+							"type": "photo",
+							"sizes": {
+								"small": {
+									"w": 680,
+									"h": 227,
+									"resize": "fit"
+								},
+								"thumb": {
+									"w": 150,
+									"h": 150,
+									"resize": "crop"
+								},
+								"medium": {
+									"w": 1200,
+									"h": 401,
+									"resize": "fit"
+								},
+								"large": {
+									"w": 1250,
+									"h": 418,
+									"resize": "fit"
+								}
+							}
+						}
+					]
+				},
+				"extended_entities": {
+					"media": [
+						{
+							"id": 781886865909682200,
+							"id_str": "781886865909682177",
+							"indices": [
+								54,
+								77
+							],
+							"media_url": "http://pbs.twimg.com/media/CtnR9kEXEAEIFQI.jpg",
+							"media_url_https": "https://pbs.twimg.com/media/CtnR9kEXEAEIFQI.jpg",
+							"url": "https://t.co/rwaHCDpuf5",
+							"display_url": "pic.twitter.com/rwaHCDpuf5",
+							"expanded_url": "https://twitter.com/JessicaValenti/status/781886874436636672/photo/1",
+							"type": "photo",
+							"sizes": {
+								"small": {
+									"w": 680,
+									"h": 227,
+									"resize": "fit"
+								},
+								"thumb": {
+									"w": 150,
+									"h": 150,
+									"resize": "crop"
+								},
+								"medium": {
+									"w": 1200,
+									"h": 401,
+									"resize": "fit"
+								},
+								"large": {
+									"w": 1250,
+									"h": 418,
+									"resize": "fit"
+								}
+							}
+						}
+					]
+				},
+				"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+				"in_reply_to_status_id": null,
+				"in_reply_to_status_id_str": null,
+				"in_reply_to_user_id": null,
+				"in_reply_to_user_id_str": null,
+				"in_reply_to_screen_name": null,
+				"user": {
+					"id": 4099171,
+					"id_str": "4099171",
+					"name": "Jessica Valenti",
+					"screen_name": "JessicaValenti",
+					"location": "Brooklyn, NY",
+					"description": "Feminist author, @GuardianUS columnist, native NYer | I only get notifications from people I follow so if you need to get in touch, try FB",
+					"url": "http://t.co/4O5BBSm35Q",
+					"entities": {
+						"url": {
+							"urls": [
+								{
+									"url": "http://t.co/4O5BBSm35Q",
+									"expanded_url": "http://jessicavalenti.com",
+									"display_url": "jessicavalenti.com",
+									"indices": [
+										0,
+										22
+									]
+								}
+							]
+						},
+						"description": {
+							"urls": []
+						}
+					},
+					"protected": false,
+					"followers_count": 134014,
+					"friends_count": 1417,
+					"listed_count": 3674,
+					"created_at": "Wed Apr 11 00:45:08 +0000 2007",
+					"favourites_count": 16348,
+					"utc_offset": -14400,
+					"time_zone": "Eastern Time (US & Canada)",
+					"geo_enabled": true,
+					"verified": true,
+					"statuses_count": 29275,
+					"lang": "en",
+					"contributors_enabled": false,
+					"is_translator": false,
+					"is_translation_enabled": false,
+					"profile_background_color": "B0A6B0",
+					"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000022435908/a436eab8d2c072ea0bcc8b92ac00257c.jpeg",
+					"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000022435908/a436eab8d2c072ea0bcc8b92ac00257c.jpeg",
+					"profile_background_tile": false,
+					"profile_image_url": "http://pbs.twimg.com/profile_images/737636068456042496/yWc4akUK_normal.jpg",
+					"profile_image_url_https": "https://pbs.twimg.com/profile_images/737636068456042496/yWc4akUK_normal.jpg",
+					"profile_banner_url": "https://pbs.twimg.com/profile_banners/4099171/1469133135",
+					"profile_link_color": "FF006E",
+					"profile_sidebar_border_color": "FFFFFF",
+					"profile_sidebar_fill_color": "F0FDF9",
+					"profile_text_color": "735DB9",
+					"profile_use_background_image": false,
+					"has_extended_profile": false,
+					"default_profile": false,
+					"default_profile_image": false,
+					"following": false,
+					"follow_request_sent": false,
+					"notifications": false
+				},
+				"geo": null,
+				"coordinates": null,
+				"place": null,
+				"contributors": null,
+				"is_quote_status": false,
+				"retweet_count": 4956,
+				"favorite_count": 5041,
+				"favorited": false,
+				"retweeted": false,
+				"possibly_sensitive": false,
+				"lang": "en"
+			},
+			"retweet_count": 1462,
+			"favorite_count": 1397,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": true,
+		"quoted_status_id": 781886874436636700,
+		"quoted_status_id_str": "781886874436636672",
+		"retweet_count": 1462,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	}
+]
