a0d005b763ec1a306d6ccbf3006ae985440aa6c0
Tim Klapdor <tim.klapdor@gmail.com>
2017-01-05 11:17:13 +1100

    Added 462 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11380,
-	"created_at": "Wed Jan 04 00:17:12 +0000 2017",
+	"tweets": 11381,
+	"created_at": "Thu Jan 05 00:17:12 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_01.js
+++ data/js/tweets/2017_01.js
@@ -1,6 +1,140 @@
 Grailbird.data.tweets_2017_01 = 
 [
 	{
+		"created_at": "Wed Jan 04 13:40:53 +0000 2017",
+		"id": 816640548802957300,
+		"id_str": "816640548802957312",
+		"text": "@anrikard weird - just watched it with my wife. Same conflicted messages",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "anrikard",
+					"name": "Andrew Rikard",
+					"id": 493538250,
+					"id_str": "493538250",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 816633101245890600,
+		"in_reply_to_status_id_str": "816633101245890560",
+		"in_reply_to_user_id": 493538250,
+		"in_reply_to_user_id_str": "493538250",
+		"in_reply_to_screen_name": "anrikard",
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Wagga Wagga",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology.",
+			"url": "http://t.co/uC0c3me6KE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/uC0c3me6KE",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 698,
+			"friends_count": 167,
+			"listed_count": 95,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 6406,
+			"utc_offset": 39600,
+			"time_zone": "Sydney",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11392,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/378800000014291852/792df4c6e7490b807e8405ea6be45833.jpeg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/378800000014291852/792df4c6e7490b807e8405ea6be45833.jpeg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/723129079671615492/O_NQ9rKL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/723129079671615492/O_NQ9rKL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "4fcb65e56a9ff434",
+			"url": "https://api.twitter.com/1.1/geo/id/4fcb65e56a9ff434.json",
+			"place_type": "neighborhood",
+			"name": "Clareville",
+			"full_name": "Clareville, Sydney",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							151.30460988,
+							-33.642468
+						],
+						[
+							151.31670192,
+							-33.642468
+						],
+						[
+							151.31670192,
+							-33.62862996
+						],
+						[
+							151.30460988,
+							-33.62862996
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Jan 03 21:45:39 +0000 2017",
 		"id": 816400153321930800,
 		"id_str": "816400153321930752",
@@ -56,7 +190,7 @@ Grailbird.data.tweets_2017_01 =
 				}
 			},
 			"protected": false,
-			"followers_count": 699,
+			"followers_count": 698,
 			"friends_count": 167,
 			"listed_count": 95,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
@@ -65,7 +199,7 @@ Grailbird.data.tweets_2017_01 =
 			"time_zone": "Sydney",
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 11391,
+			"statuses_count": 11392,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -139,16 +273,16 @@ Grailbird.data.tweets_2017_01 =
 					}
 				},
 				"protected": false,
-				"followers_count": 10111,
+				"followers_count": 10116,
 				"friends_count": 282,
 				"listed_count": 397,
 				"created_at": "Wed Feb 11 18:11:52 +0000 2009",
-				"favourites_count": 4391,
+				"favourites_count": 4395,
 				"utc_offset": -18000,
 				"time_zone": "Eastern Time (US & Canada)",
 				"geo_enabled": true,
 				"verified": false,
-				"statuses_count": 20670,
+				"statuses_count": 20678,
 				"lang": "en",
 				"contributors_enabled": false,
 				"is_translator": false,
@@ -178,14 +312,14 @@ Grailbird.data.tweets_2017_01 =
 			"place": null,
 			"contributors": null,
 			"is_quote_status": false,
-			"retweet_count": 10,
-			"favorite_count": 29,
+			"retweet_count": 12,
+			"favorite_count": 32,
 			"favorited": false,
 			"retweeted": true,
 			"lang": "en"
 		},
 		"is_quote_status": false,
-		"retweet_count": 10,
+		"retweet_count": 12,
 		"favorite_count": 0,
 		"favorited": false,
 		"retweeted": true,
@@ -247,7 +381,7 @@ Grailbird.data.tweets_2017_01 =
 				}
 			},
 			"protected": false,
-			"followers_count": 699,
+			"followers_count": 698,
 			"friends_count": 167,
 			"listed_count": 95,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
@@ -256,7 +390,7 @@ Grailbird.data.tweets_2017_01 =
 			"time_zone": "Sydney",
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 11391,
+			"statuses_count": 11392,
 			"lang": "en",
 			"contributors_enabled": false,
 			"is_translator": false,
