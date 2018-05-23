17227e6fd2ceb47956afde97d642e4a84d8d5978
Tim Klapdor <tim.klapdor@gmail.com>
2018-05-23 10:17:28 +1000

    Added 2015 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_05.js",
 		"year": 2018,
 		"var_name": "tweets_2018_05",
-		"tweet_count": 68,
+		"tweet_count": 70,
 		"month": 5
 	},
 	{
@@ -38,7 +38,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_12.js",
 		"year": 2017,
 		"var_name": "tweets_2017_12",
-		"tweet_count": 78,
+		"tweet_count": 77,
 		"month": 12
 	},
 	{
