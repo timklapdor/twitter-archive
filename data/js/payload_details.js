5840a04e4dbb03f15967dddc5f4cbce6c0a2cbb7
Tim Klapdor <tim.klapdor@gmail.com>
2018-08-25 10:17:20 +1000

    Added 2266 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13181,
-	"created_at": "Fri Aug 24 00:17:20 +0000 2018",
+	"tweets": 13185,
+	"created_at": "Sat Aug 25 00:17:19 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_08.js",
 		"year": 2018,
 		"var_name": "tweets_2018_08",
-		"tweet_count": 71,
+		"tweet_count": 76,
 		"month": 8
 	},
 	{
@@ -24,7 +24,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_05.js",
 		"year": 2018,
 		"var_name": "tweets_2018_05",
-		"tweet_count": 99,
+		"tweet_count": 98,
 		"month": 5
 	},
 	{
