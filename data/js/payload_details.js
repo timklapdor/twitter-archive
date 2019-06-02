27b873e7859f55c9d9a24994e08cb49f123a3f42
Tim Klapdor <tim.klapdor@gmail.com>
2019-06-02 10:17:28 +1000

    Added 2818 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13738,
-	"created_at": "Sat Jun 01 00:17:23 +0000 2019",
+	"tweets": 13737,
+	"created_at": "Sun Jun 02 00:17:27 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -31,7 +31,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2019,
 		"var_name": "tweets_2019_01",
-		"tweet_count": 90,
+		"tweet_count": 89,
 		"month": 1
 	},
 	{
