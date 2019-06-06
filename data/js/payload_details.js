35d13f5e5c2259cf7e010ef5e65609dc464ef291
Tim Klapdor <tim.klapdor@gmail.com>
2019-06-06 10:17:31 +1000

    Added 2823 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13741,
-	"created_at": "Wed Jun 05 00:17:23 +0000 2019",
+	"tweets": 13742,
+	"created_at": "Thu Jun 06 00:17:30 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Wed Jun 05 00:50:25 +0000 2019",
+		"id": 1136062484299698200,
+		"id_str": "1136072749686767617",
+		"full_text": "The best take on the ANU data breach https://t.co/2KkjCuzTXf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1136062458525700100,
+					"id_str": "1136062458525700096",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/D8QayXMU0AAjk2j.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8QayXMU0AAjk2j.jpg",
+					"url": "https://t.co/2KkjCuzTXf",
+					"display_url": "pic.twitter.com/2KkjCuzTXf",
+					"expanded_url": "https://twitter.com/mmmeasterbrook/status/1136062484299698176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 319,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1148,
+							"h": 538,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1148,
+							"h": 538,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136062458525700100,
+					"id_str": "1136062458525700096",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/D8QayXMU0AAjk2j.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8QayXMU0AAjk2j.jpg",
+					"url": "https://t.co/2KkjCuzTXf",
+					"display_url": "pic.twitter.com/2KkjCuzTXf",
+					"expanded_url": "https://twitter.com/mmmeasterbrook/status/1136062484299698176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 319,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1148,
+							"h": 538,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1148,
+							"h": 538,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1005688251040927700,
+			"id_str": "1005688251040927745",
+			"name": "Maddalena Easterbrook",
+			"screen_name": "mmmeasterbrook",
+			"location": "Ngunnawal Country",
+			"description": "Law student at the ANU. Working in media.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 138,
+			"friends_count": 2112,
+			"listed_count": 0,
+			"created_at": "Sun Jun 10 05:48:58 +0000 2018",
+			"favourites_count": 6226,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 579,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117314850495979520/TUhdH1ss_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117314850495979520/TUhdH1ss_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1005688251040927745/1546494563",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 92,
+		"favorite_count": 274,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The best take on the ANU data breach https://t.co/2KkjCuzTXf"
+	},
+	{
 		"created_at": "Tue Jun 04 03:24:21 +0000 2019",
 		"id": 1135749099993542700,
 		"id_str": "1135749099993542657",
@@ -64,12 +225,12 @@ Grailbird.data.tweets_2019_06 =
 			"friends_count": 141,
 			"listed_count": 95,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 6744,
+			"favourites_count": 6751,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 13525,
+			"statuses_count": 13526,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -280,12 +441,12 @@ Grailbird.data.tweets_2019_06 =
 			"friends_count": 141,
 			"listed_count": 95,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 6744,
+			"favourites_count": 6751,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 13525,
+			"statuses_count": 13526,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -315,7 +476,7 @@ Grailbird.data.tweets_2019_06 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 2,
+		"retweet_count": 3,
 		"favorite_count": 4,
 		"favorited": false,
 		"retweeted": false,
@@ -397,12 +558,12 @@ Grailbird.data.tweets_2019_06 =
 			"friends_count": 141,
 			"listed_count": 95,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 6744,
+			"favourites_count": 6751,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 13525,
+			"statuses_count": 13526,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -556,12 +717,12 @@ Grailbird.data.tweets_2019_06 =
 			"friends_count": 141,
 			"listed_count": 95,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 6744,
+			"favourites_count": 6751,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 13525,
+			"statuses_count": 13526,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -647,16 +808,16 @@ Grailbird.data.tweets_2019_06 =
 				}
 			},
 			"protected": false,
-			"followers_count": 3694,
+			"followers_count": 3693,
 			"friends_count": 577,
 			"listed_count": 414,
 			"created_at": "Mon Sep 10 19:58:43 +0000 2007",
-			"favourites_count": 11668,
+			"favourites_count": 11672,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": false,
 			"verified": false,
-			"statuses_count": 28884,
+			"statuses_count": 28888,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
