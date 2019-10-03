4768d468a51f3bfeeabf80236cd09728951d1a7a
Tim Klapdor <tim.klapdor@gmail.com>
2019-10-03 09:47:27 +0930

    Added 2985 tweets

---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Wed Oct 02 21:37:45 +0000 2019",
+		"id": 1179398380340093000,
+		"id_str": "1179510807635218432",
+		"full_text": "In the desperate search for what works at scale in HE, seems that learning design has become distilled into facilitating a colourful cardsort. Rather than investing in methods invest in people with the deep knowledge &amp; expertise to work with educators in the design of learning",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			281
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 859745605505175600,
+			"id_str": "859745605505175552",
+			"name": "Neil Mosley",
+			"screen_name": "neilmosley5",
+			"location": "Cardiff, Wales",
+			"description": "Digital / online learning designer, strategist & producer | Cardiff University | Previously Imperial College | Instructional Design | UX. Own views",
+			"url": "https://t.co/NpoQidXy3g",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/NpoQidXy3g",
+							"expanded_url": "https://www.linkedin.com/in/neil-mosley-34a93890/",
+							"display_url": "linkedin.com/in/neil-mosley…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1778,
+			"friends_count": 4875,
+			"listed_count": 23,
+			"created_at": "Wed May 03 12:25:00 +0000 2017",
+			"favourites_count": 5449,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4774,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/859762487184560128/M6vfWwHo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/859762487184560128/M6vfWwHo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/859745605505175552/1540851994",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "In the desperate search for what works at scale in HE, seems that learning design has become distilled into facilitating a colourful cardsort. Rather than investing in methods invest in people with the deep knowledge &amp; expertise to work with educators in the design of learning"
+	}
+]
