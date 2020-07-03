a48e1fd117fcab48eb9841bc601a3364d3aab073
Tim Klapdor <tim.klapdor@gmail.com>
2020-07-03 09:47:45 +0930

    Added 3145 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14063,
-	"created_at": "Thu Jul 02 00:17:44 +0000 2020",
+	"tweets": 14064,
+	"created_at": "Fri Jul 03 00:17:43 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 02 11:36:46 +0000 2020",
+		"id": 1278653841236324400,
+		"id_str": "1278653841236324357",
+		"full_text": "@shalailah You and Alise both ;)",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "shalailah",
+					"name": "Shalailah Medhora",
+					"id": 46828208,
+					"id_str": "46828208",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1278593934785052700,
+		"in_reply_to_status_id_str": "1278593934785052672",
+		"in_reply_to_user_id": 46828208,
+		"in_reply_to_user_id_str": "46828208",
+		"in_reply_to_screen_name": "shalailah",
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Adelaide, South Australia",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology.",
+			"url": "https://t.co/uC0c3me6KE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uC0c3me6KE",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 830,
+			"friends_count": 158,
+			"listed_count": 94,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 8063,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14367,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "7b46944933d8137e",
+			"url": "https://api.twitter.com/1.1/geo/id/7b46944933d8137e.json",
+			"place_type": "neighborhood",
+			"name": "Norwood",
+			"full_name": "Norwood, Adelaide",
+			"country_code": "AU",
+			"country": "Australia",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							138.62264616,
+							-34.92790299
+						],
+						[
+							138.64147308,
+							-34.92790299
+						],
+						[
+							138.64147308,
+							-34.91480097
+						],
+						[
+							138.62264616,
+							-34.91480097
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@shalailah You and Alise both ;)"
+	},
+	{
+		"created_at": "Thu Jul 02 00:41:18 +0000 2020",
+		"id": 1278202477616062500,
+		"id_str": "1278488886038024193",
+		"full_text": "Instead of the $270b military spend, we could:\n✔give free uni and TAFE to all who wanted it\n✔offer every parent free childcare\n✔bring dental into Medicare (six times over)\n✔build the 500,000 public homes needed to end homelessness (twenty times over)\n\nWhat would you rather?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 25960714,
+			"id_str": "25960714",
+			"name": "Adam Bandt",
+			"screen_name": "AdamBandt",
+			"location": "Melbourne, Victoria. Authorised by Adam Bandt, The Australian Greens, 296 Brunswick St, Fitzroy, 3065.",
+			"description": "MP for Melbourne. Leader of the Australian Greens.",
+			"url": "https://t.co/blShpvRx3t",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/blShpvRx3t",
+							"expanded_url": "http://adambandt.com",
+							"display_url": "adambandt.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 192609,
+			"friends_count": 19228,
+			"listed_count": 1468,
+			"created_at": "Mon Mar 23 05:39:19 +0000 2009",
+			"favourites_count": 1105,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13693,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234768376586633217/bN2_AA3S_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234768376586633217/bN2_AA3S_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/25960714/1535506181",
+			"profile_link_color": "119E32",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2459,
+		"favorite_count": 7766,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Instead of the $270b military spend, we could:\n✔give free uni and TAFE to all who wanted it\n✔offer every parent free childcare\n✔bring dental into Medicare (six times over)\n✔build the 500,000 public homes needed to end homelessness (twenty times over)\n\nWhat would you rather?"
+	},
+	{
 		"created_at": "Wed Jul 01 14:14:38 +0000 2020",
 		"id": 1278226106944942000,
 		"id_str": "1278331182845882369",
@@ -127,16 +361,16 @@ Grailbird.data.tweets_2020_07 =
 				}
 			},
 			"protected": false,
-			"followers_count": 29773,
+			"followers_count": 29778,
 			"friends_count": 2228,
 			"listed_count": 308,
 			"created_at": "Tue Jan 11 06:02:39 +0000 2011",
-			"favourites_count": 3102,
+			"favourites_count": 3104,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": true,
-			"statuses_count": 11592,
+			"statuses_count": 11598,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -166,8 +400,8 @@ Grailbird.data.tweets_2020_07 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 273,
-		"favorite_count": 747,
+		"retweet_count": 322,
+		"favorite_count": 861,
 		"favorited": false,
 		"retweeted": true,
 		"possibly_sensitive": false,
@@ -223,16 +457,16 @@ Grailbird.data.tweets_2020_07 =
 				}
 			},
 			"protected": false,
-			"followers_count": 18332,
+			"followers_count": 18344,
 			"friends_count": 2991,
 			"listed_count": 358,
 			"created_at": "Mon May 24 01:26:34 +0000 2010",
-			"favourites_count": 34524,
+			"favourites_count": 34548,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": false,
 			"verified": true,
-			"statuses_count": 71797,
+			"statuses_count": 71824,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -262,8 +496,8 @@ Grailbird.data.tweets_2020_07 =
 		"place": null,
 		"contributors": null,
 		"is_quote_status": false,
-		"retweet_count": 877,
-		"favorite_count": 2308,
+		"retweet_count": 947,
+		"favorite_count": 2529,
 		"favorited": false,
 		"retweeted": true,
 		"lang": "en",
@@ -333,12 +567,12 @@ Grailbird.data.tweets_2020_07 =
 			"friends_count": 158,
 			"listed_count": 94,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 8056,
+			"favourites_count": 8063,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 14365,
+			"statuses_count": 14367,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -467,7 +701,7 @@ Grailbird.data.tweets_2020_07 =
 				}
 			},
 			"protected": false,
-			"followers_count": 38342,
+			"followers_count": 38370,
 			"friends_count": 486,
 			"listed_count": 281,
 			"created_at": "Mon Apr 30 12:38:04 +0000 2012",
@@ -476,7 +710,7 @@ Grailbird.data.tweets_2020_07 =
 			"time_zone": null,
 			"geo_enabled": false,
 			"verified": false,
-			"statuses_count": 4442,
+			"statuses_count": 4443,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -507,7 +741,7 @@ Grailbird.data.tweets_2020_07 =
 		"contributors": null,
 		"is_quote_status": false,
 		"retweet_count": 48,
-		"favorite_count": 311,
+		"favorite_count": 309,
 		"favorited": true,
 		"retweeted": true,
 		"possibly_sensitive": false,
