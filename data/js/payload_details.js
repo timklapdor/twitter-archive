566f913416f42ad57aaa3081a5d35733f2d251e9
Tim Klapdor <tim.klapdor@gmail.com>
2020-11-13 10:47:52 +1030

    Added 3139 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
 	"tweets": 14058,
-	"created_at": "Thu Nov 12 00:17:48 +0000 2020",
+	"created_at": "Fri Nov 13 00:17:50 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_11.js",
 		"year": 2020,
 		"var_name": "tweets_2020_11",
-		"tweet_count": 28,
+		"tweet_count": 30,
 		"month": 11
 	},
 	{
@@ -304,7 +304,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_04.js",
 		"year": 2017,
 		"var_name": "tweets_2017_04",
-		"tweet_count": 30,
+		"tweet_count": 28,
 		"month": 4
 	},
 	{
