721c19dd7c26a8e797726c0a7d3a088cf54f2502
Tim Klapdor <tim.klapdor@gmail.com>
2021-03-28 10:47:23 +1030

    Added 3138 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_03.js",
 		"year": 2021,
 		"var_name": "tweets_2021_03",
-		"tweet_count": 66,
+		"tweet_count": 68,
 		"month": 3
 	},
 	{
@@ -17,14 +17,14 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_01.js",
 		"year": 2021,
 		"var_name": "tweets_2021_01",
-		"tweet_count": 75,
+		"tweet_count": 74,
 		"month": 1
 	},
 	{
 		"file_name": "data/js/tweets/2020_12.js",
 		"year": 2020,
 		"var_name": "tweets_2020_12",
-		"tweet_count": 63,
+		"tweet_count": 64,
 		"month": 12
 	},
 	{
@@ -94,21 +94,21 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_02.js",
 		"year": 2020,
 		"var_name": "tweets_2020_02",
-		"tweet_count": 59,
+		"tweet_count": 58,
 		"month": 2
 	},
 	{
 		"file_name": "data/js/tweets/2020_01.js",
 		"year": 2020,
 		"var_name": "tweets_2020_01",
-		"tweet_count": 124,
+		"tweet_count": 125,
 		"month": 1
 	},
 	{
 		"file_name": "data/js/tweets/2019_12.js",
 		"year": 2019,
 		"var_name": "tweets_2019_12",
-		"tweet_count": 61,
+		"tweet_count": 62,
 		"month": 12
 	},
 	{
@@ -311,7 +311,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_07.js",
 		"year": 2017,
 		"var_name": "tweets_2017_07",
-		"tweet_count": 5,
+		"tweet_count": 3,
 		"month": 7
 	},
 	{
--- data/js/tweets/2017_07.js
+++ data/js/tweets/2017_07.js
@@ -83,16 +83,16 @@ Grailbird.data.tweets_2017_07 =
 				}
 			},
 			"protected": false,
-			"followers_count": 826,
+			"followers_count": 825,
 			"friends_count": 148,
 			"listed_count": 91,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 8899,
+			"favourites_count": 8900,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 14846,
+			"statuses_count": 14848,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -188,16 +188,16 @@ Grailbird.data.tweets_2017_07 =
 					}
 				},
 				"protected": false,
-				"followers_count": 3317,
-				"friends_count": 3817,
+				"followers_count": 3318,
+				"friends_count": 3818,
 				"listed_count": 148,
 				"created_at": "Tue Mar 03 20:30:51 +0000 2009",
-				"favourites_count": 27931,
+				"favourites_count": 27948,
 				"utc_offset": null,
 				"time_zone": null,
 				"geo_enabled": false,
 				"verified": false,
-				"statuses_count": 23657,
+				"statuses_count": 23667,
 				"lang": null,
 				"contributors_enabled": false,
 				"is_translator": false,
@@ -301,16 +301,16 @@ Grailbird.data.tweets_2017_07 =
 				}
 			},
 			"protected": false,
-			"followers_count": 3317,
-			"friends_count": 3817,
+			"followers_count": 3318,
+			"friends_count": 3818,
 			"listed_count": 148,
 			"created_at": "Tue Mar 03 20:30:51 +0000 2009",
-			"favourites_count": 27931,
+			"favourites_count": 27948,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": false,
 			"verified": false,
-			"statuses_count": 23657,
+			"statuses_count": 23667,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -418,16 +418,16 @@ Grailbird.data.tweets_2017_07 =
 				}
 			},
 			"protected": false,
-			"followers_count": 826,
+			"followers_count": 825,
 			"friends_count": 148,
 			"listed_count": 91,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 8899,
+			"favourites_count": 8900,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 14846,
+			"statuses_count": 14848,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -463,417 +463,5 @@ Grailbird.data.tweets_2017_07 =
 		"retweeted": false,
 		"lang": "en",
 		"text": "@jimgroom @rowan_peter You did good Garry ;-)"
-	},
-	{
-		"created_at": "Sat Jul 29 03:49:36 +0000 2017",
-		"id": 891143646665293800,
-		"id_str": "891143646665293825",
-		"full_text": "This was soooo cool. Before TV I reckon people came from miles around to watch this thing in action :-) https://t.co/U9ApTQsz81",
-		"truncated": false,
-		"display_text_range": [
-			0,
-			103
-		],
-		"entities": {
-			"hashtags": [],
-			"symbols": [],
-			"user_mentions": [],
-			"urls": [
-				{
-					"url": "https://t.co/U9ApTQsz81",
-					"expanded_url": "https://twitter.com/jimgroom/status/891142428584886272",
-					"display_url": "twitter.com/jimgroom/statu…",
-					"indices": [
-						104,
-						127
-					]
-				}
-			]
-		},
-		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
-		"in_reply_to_status_id": null,
-		"in_reply_to_status_id_str": null,
-		"in_reply_to_user_id": null,
-		"in_reply_to_user_id_str": null,
-		"in_reply_to_screen_name": null,
-		"user": {
-			"id": 174124175,
-			"id_str": "174124175",
-			"name": "Tim Klapdor",
-			"screen_name": "timklapdor",
-			"location": "Adelaide, South Australia",
-			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology.",
-			"url": "https://t.co/uC0c3me6KE",
-			"entities": {
-				"url": {
-					"urls": [
-						{
-							"url": "https://t.co/uC0c3me6KE",
-							"expanded_url": "http://timklapdor.com",
-							"display_url": "timklapdor.com",
-							"indices": [
-								0,
-								23
-							]
-						}
-					]
-				},
-				"description": {
-					"urls": []
-				}
-			},
-			"protected": false,
-			"followers_count": 826,
-			"friends_count": 148,
-			"listed_count": 91,
-			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 8899,
-			"utc_offset": null,
-			"time_zone": null,
-			"geo_enabled": true,
-			"verified": false,
-			"statuses_count": 14846,
-			"lang": null,
-			"contributors_enabled": false,
-			"is_translator": false,
-			"is_translation_enabled": false,
-			"profile_background_color": "C0DEED",
-			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_tile": false,
-			"profile_image_url": "http://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
-			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
-			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
-			"profile_link_color": "5FA832",
-			"profile_sidebar_border_color": "000000",
-			"profile_sidebar_fill_color": "DDEEF6",
-			"profile_text_color": "333333",
-			"profile_use_background_image": true,
-			"has_extended_profile": true,
-			"default_profile": false,
-			"default_profile_image": false,
-			"following": false,
-			"follow_request_sent": false,
-			"notifications": false,
-			"translator_type": "none"
-		},
-		"geo": null,
-		"coordinates": null,
-		"place": null,
-		"contributors": null,
-		"is_quote_status": true,
-		"quoted_status_id": 891142428584886300,
-		"quoted_status_id_str": "891142428584886272",
-		"quoted_status_permalink": {
-			"url": "https://t.co/U9ApTQsz81",
-			"expanded": "https://twitter.com/jimgroom/status/891142428584886272",
-			"display": "twitter.com/jimgroom/statu…"
-		},
-		"quoted_status": {
-			"created_at": "Sat Jul 29 03:44:46 +0000 2017",
-			"id": 891142428584886300,
-			"id_str": "891142428584886272",
-			"full_text": "A look at a 1909 \"cash register\" at the Up-to-date Store in Coolamon. Money (or IOU) travelled in a ball to bookkeeper on a suspended track. https://t.co/W6Ta9AxlUa",
-			"truncated": false,
-			"display_text_range": [
-				0,
-				140
-			],
-			"entities": {
-				"hashtags": [],
-				"symbols": [],
-				"user_mentions": [],
-				"urls": [],
-				"media": [
-					{
-						"id": 891142096442114000,
-						"id_str": "891142096442114049",
-						"indices": [
-							141,
-							164
-						],
-						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/891142096442114049/pu/img/bCPbHBR19XxhG462.jpg",
-						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/891142096442114049/pu/img/bCPbHBR19XxhG462.jpg",
-						"url": "https://t.co/W6Ta9AxlUa",
-						"display_url": "pic.twitter.com/W6Ta9AxlUa",
-						"expanded_url": "https://twitter.com/jimgroom/status/891142428584886272/video/1",
-						"type": "photo",
-						"sizes": {
-							"thumb": {
-								"w": 150,
-								"h": 150,
-								"resize": "crop"
-							},
-							"medium": {
-								"w": 1200,
-								"h": 675,
-								"resize": "fit"
-							},
-							"small": {
-								"w": 680,
-								"h": 383,
-								"resize": "fit"
-							},
-							"large": {
-								"w": 1280,
-								"h": 720,
-								"resize": "fit"
-							}
-						}
-					}
-				]
-			},
-			"extended_entities": {
-				"media": [
-					{
-						"id": 891142096442114000,
-						"id_str": "891142096442114049",
-						"indices": [
-							141,
-							164
-						],
-						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/891142096442114049/pu/img/bCPbHBR19XxhG462.jpg",
-						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/891142096442114049/pu/img/bCPbHBR19XxhG462.jpg",
-						"url": "https://t.co/W6Ta9AxlUa",
-						"display_url": "pic.twitter.com/W6Ta9AxlUa",
-						"expanded_url": "https://twitter.com/jimgroom/status/891142428584886272/video/1",
-						"type": "video",
-						"sizes": {
-							"thumb": {
-								"w": 150,
-								"h": 150,
-								"resize": "crop"
-							},
-							"medium": {
-								"w": 1200,
-								"h": 675,
-								"resize": "fit"
-							},
-							"small": {
-								"w": 680,
-								"h": 383,
-								"resize": "fit"
-							},
-							"large": {
-								"w": 1280,
-								"h": 720,
-								"resize": "fit"
-							}
-						},
-						"video_info": {
-							"aspect_ratio": [
-								16,
-								9
-							],
-							"duration_millis": 27700,
-							"variants": [
-								{
-									"bitrate": 832000,
-									"content_type": "video/mp4",
-									"url": "https://video.twimg.com/ext_tw_video/891142096442114049/pu/vid/640x360/4jRSHce-mxtiOjiY.mp4"
-								},
-								{
-									"content_type": "application/x-mpegURL",
-									"url": "https://video.twimg.com/ext_tw_video/891142096442114049/pu/pl/Tt-1CctSXmktTIzq.m3u8"
-								},
-								{
-									"bitrate": 2176000,
-									"content_type": "video/mp4",
-									"url": "https://video.twimg.com/ext_tw_video/891142096442114049/pu/vid/1280x720/Cr0HOa86eGPFHUwT.mp4"
-								},
-								{
-									"bitrate": 256000,
-									"content_type": "video/mp4",
-									"url": "https://video.twimg.com/ext_tw_video/891142096442114049/pu/vid/320x180/OaPvpFEC8TfqvKkM.mp4"
-								}
-							]
-						},
-						"additional_media_info": {
-							"monetizable": false
-						}
-					}
-				]
-			},
-			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
-			"in_reply_to_status_id": null,
-			"in_reply_to_status_id_str": null,
-			"in_reply_to_user_id": null,
-			"in_reply_to_user_id_str": null,
-			"in_reply_to_screen_name": null,
-			"user": {
-				"id": 3362981,
-				"id_str": "3362981",
-				"name": "Jim Groom",
-				"screen_name": "jimgroom",
-				"location": "supine",
-				"description": "a b twit",
-				"url": "https://t.co/qHimKmPXIO",
-				"entities": {
-					"url": {
-						"urls": [
-							{
-								"url": "https://t.co/qHimKmPXIO",
-								"expanded_url": "http://bavatuesdays.com",
-								"display_url": "bavatuesdays.com",
-								"indices": [
-									0,
-									23
-								]
-							}
-						]
-					},
-					"description": {
-						"urls": []
-					}
-				},
-				"protected": false,
-				"followers_count": 5563,
-				"friends_count": 2340,
-				"listed_count": 508,
-				"created_at": "Tue Apr 03 21:20:45 +0000 2007",
-				"favourites_count": 149,
-				"utc_offset": null,
-				"time_zone": null,
-				"geo_enabled": true,
-				"verified": false,
-				"statuses_count": 64159,
-				"lang": null,
-				"contributors_enabled": false,
-				"is_translator": false,
-				"is_translation_enabled": false,
-				"profile_background_color": "9AE4E8",
-				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-				"profile_background_tile": true,
-				"profile_image_url": "http://pbs.twimg.com/profile_images/626944793302581248/TxzPTAYL_normal.jpg",
-				"profile_image_url_https": "https://pbs.twimg.com/profile_images/626944793302581248/TxzPTAYL_normal.jpg",
-				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3362981/1438310580",
-				"profile_link_color": "0000FF",
-				"profile_sidebar_border_color": "87BC44",
-				"profile_sidebar_fill_color": "E0FF92",
-				"profile_text_color": "000000",
-				"profile_use_background_image": true,
-				"has_extended_profile": false,
-				"default_profile": false,
-				"default_profile_image": false,
-				"following": true,
-				"follow_request_sent": false,
-				"notifications": false,
-				"translator_type": "none"
-			},
-			"geo": null,
-			"coordinates": null,
-			"place": null,
-			"contributors": null,
-			"is_quote_status": false,
-			"retweet_count": 2,
-			"favorite_count": 3,
-			"favorited": false,
-			"retweeted": false,
-			"possibly_sensitive": false,
-			"lang": "en"
-		},
-		"retweet_count": 0,
-		"favorite_count": 0,
-		"favorited": false,
-		"retweeted": false,
-		"possibly_sensitive": false,
-		"lang": "en",
-		"text": "This was soooo cool. Before TV I reckon people came from miles around to watch this thing in action :-) https://t.co/U9ApTQsz81"
-	},
-	{
-		"created_at": "Sat Jul 29 03:33:07 +0000 2017",
-		"id": 759827631449702400,
-		"id_str": "891139496711921664",
-		"full_text": "Reince Priebus sounds like Scooby Doo saying nice penis",
-		"truncated": false,
-		"display_text_range": [
-			0,
-			55
-		],
-		"entities": {
-			"hashtags": [],
-			"symbols": [],
-			"user_mentions": [],
-			"urls": []
-		},
-		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
-		"in_reply_to_status_id": null,
-		"in_reply_to_status_id_str": null,
-		"in_reply_to_user_id": null,
-		"in_reply_to_user_id_str": null,
-		"in_reply_to_screen_name": null,
-		"user": {
-			"id": 70775995,
-			"id_str": "70775995",
-			"name": "Mark Agee",
-			"screen_name": "MarkAgee",
-			"location": "L.A. via Virginia & Texas",
-			"description": "Comedy writer for some TV shows, Netflix/Comedy Central/Tru/TNT etc. Guy who has rage fits during mini golf.",
-			"url": "https://t.co/zVQm0owC3e",
-			"entities": {
-				"url": {
-					"urls": [
-						{
-							"url": "https://t.co/zVQm0owC3e",
-							"expanded_url": "http://www.imdb.com/name/nm3140510/",
-							"display_url": "imdb.com/name/nm3140510/",
-							"indices": [
-								0,
-								23
-							]
-						}
-					]
-				},
-				"description": {
-					"urls": []
-				}
-			},
-			"protected": false,
-			"followers_count": 31410,
-			"friends_count": 2071,
-			"listed_count": 491,
-			"created_at": "Tue Sep 01 20:24:37 +0000 2009",
-			"favourites_count": 130662,
-			"utc_offset": null,
-			"time_zone": null,
-			"geo_enabled": true,
-			"verified": false,
-			"statuses_count": 61262,
-			"lang": null,
-			"contributors_enabled": false,
-			"is_translator": false,
-			"is_translation_enabled": false,
-			"profile_background_color": "FFFFFF",
-			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-			"profile_background_tile": false,
-			"profile_image_url": "http://pbs.twimg.com/profile_images/1118378818400661504/n5Kvdd4k_normal.jpg",
-			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1118378818400661504/n5Kvdd4k_normal.jpg",
-			"profile_banner_url": "https://pbs.twimg.com/profile_banners/70775995/1364851804",
-			"profile_link_color": "000000",
-			"profile_sidebar_border_color": "FFFFFF",
-			"profile_sidebar_fill_color": "DDEEF6",
-			"profile_text_color": "333333",
-			"profile_use_background_image": false,
-			"has_extended_profile": true,
-			"default_profile": false,
-			"default_profile_image": false,
-			"following": false,
-			"follow_request_sent": false,
-			"notifications": false,
-			"translator_type": "none"
-		},
-		"geo": null,
-		"coordinates": null,
-		"place": null,
-		"contributors": null,
-		"is_quote_status": false,
-		"retweet_count": 2889,
-		"favorite_count": 8780,
-		"favorited": false,
-		"retweeted": false,
-		"lang": "en",
-		"text": "Reince Priebus sounds like Scooby Doo saying nice penis"
 	}
 ]
