e857c0356607c3806bb937d439cd6e493a23381a
Tim Klapdor <tim.klapdor@gmail.com>
2021-10-10 10:47:21 +1030

    Added 3141 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14057,
-	"created_at": "Sat Oct 09 00:17:37 +0000 2021",
+	"tweets": 14060,
+	"created_at": "Sun Oct 10 00:17:19 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_10.js",
 		"year": 2021,
 		"var_name": "tweets_2021_10",
-		"tweet_count": 13,
+		"tweet_count": 14,
 		"month": 10
 	},
 	{
@@ -241,7 +241,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 58,
+		"tweet_count": 60,
 		"month": 12
 	},
 	{
