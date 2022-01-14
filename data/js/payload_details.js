bf9314a592c33349717ba0404502faf5ec7f568f
Tim Klapdor <tim.klapdor@gmail.com>
2022-01-14 10:47:17 +1030

    Added 2160 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14055,
-	"created_at": "Thu Jan 13 00:17:27 +0000 2022",
+	"tweets": 13079,
+	"created_at": "Fri Jan 14 00:17:16 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -24,7 +24,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_10.js",
 		"year": 2021,
 		"var_name": "tweets_2021_10",
-		"tweet_count": 41,
+		"tweet_count": 42,
 		"month": 10
 	},
 	{
@@ -255,91 +255,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2019,
 		"var_name": "tweets_2019_01",
-		"tweet_count": 83,
-		"month": 1
-	},
-	{
-		"file_name": "data/js/tweets/2018_12.js",
-		"year": 2018,
-		"var_name": "tweets_2018_12",
-		"tweet_count": 61,
-		"month": 12
-	},
-	{
-		"file_name": "data/js/tweets/2018_11.js",
-		"year": 2018,
-		"var_name": "tweets_2018_11",
-		"tweet_count": 52,
-		"month": 11
-	},
-	{
-		"file_name": "data/js/tweets/2018_10.js",
-		"year": 2018,
-		"var_name": "tweets_2018_10",
-		"tweet_count": 42,
-		"month": 10
-	},
-	{
-		"file_name": "data/js/tweets/2018_09.js",
-		"year": 2018,
-		"var_name": "tweets_2018_09",
-		"tweet_count": 58,
-		"month": 9
-	},
-	{
-		"file_name": "data/js/tweets/2018_08.js",
-		"year": 2018,
-		"var_name": "tweets_2018_08",
-		"tweet_count": 87,
-		"month": 8
-	},
-	{
-		"file_name": "data/js/tweets/2018_07.js",
-		"year": 2018,
-		"var_name": "tweets_2018_07",
-		"tweet_count": 91,
-		"month": 7
-	},
-	{
-		"file_name": "data/js/tweets/2018_06.js",
-		"year": 2018,
-		"var_name": "tweets_2018_06",
-		"tweet_count": 56,
-		"month": 6
-	},
-	{
-		"file_name": "data/js/tweets/2018_05.js",
-		"year": 2018,
-		"var_name": "tweets_2018_05",
-		"tweet_count": 96,
-		"month": 5
-	},
-	{
-		"file_name": "data/js/tweets/2018_04.js",
-		"year": 2018,
-		"var_name": "tweets_2018_04",
-		"tweet_count": 109,
-		"month": 4
-	},
-	{
-		"file_name": "data/js/tweets/2018_03.js",
-		"year": 2018,
-		"var_name": "tweets_2018_03",
-		"tweet_count": 103,
-		"month": 3
-	},
-	{
-		"file_name": "data/js/tweets/2018_02.js",
-		"year": 2018,
-		"var_name": "tweets_2018_02",
-		"tweet_count": 84,
-		"month": 2
-	},
-	{
-		"file_name": "data/js/tweets/2018_01.js",
-		"year": 2018,
-		"var_name": "tweets_2018_01",
-		"tweet_count": 77,
+		"tweet_count": 22,
 		"month": 1
 	},
 	{
