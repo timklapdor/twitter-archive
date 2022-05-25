c59daf46250f84767f4243e5b1d6704ec66aff11
Tim Klapdor <tim.klapdor@gmail.com>
2022-05-25 09:47:19 +0930

    Added 2749 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14058,
-	"created_at": "Tue May 24 00:17:27 +0000 2022",
+	"tweets": 13668,
+	"created_at": "Wed May 25 00:17:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -325,36 +325,8 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_07.js",
 		"year": 2018,
 		"var_name": "tweets_2018_07",
-		"tweet_count": 91,
+		"tweet_count": 39,
 		"month": 7
-	},
-	{
-		"file_name": "data/js/tweets/2018_06.js",
-		"year": 2018,
-		"var_name": "tweets_2018_06",
-		"tweet_count": 56,
-		"month": 6
-	},
-	{
-		"file_name": "data/js/tweets/2018_05.js",
-		"year": 2018,
-		"var_name": "tweets_2018_05",
-		"tweet_count": 96,
-		"month": 5
-	},
-	{
-		"file_name": "data/js/tweets/2018_04.js",
-		"year": 2018,
-		"var_name": "tweets_2018_04",
-		"tweet_count": 109,
-		"month": 4
-	},
-	{
-		"file_name": "data/js/tweets/2018_03.js",
-		"year": 2018,
-		"var_name": "tweets_2018_03",
-		"tweet_count": 77,
-		"month": 3
 	},
 	{
 		"file_name": "data/js/tweets/2016_08.js",
