6f6108a57f33511a37520104b2e49d7ebb4ce7ad
Tim Klapdor <tim.klapdor@gmail.com>
2022-06-05 09:47:25 +0930

    Added 3140 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
 	"tweets": 14059,
-	"created_at": "Sat Jun 04 00:17:19 +0000 2022",
+	"created_at": "Sun Jun 05 00:17:24 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,331 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sat Jun 04 10:51:43 +0000 2022",
+		"id": 1533038785872597000,
+		"id_str": "1533038785872596993",
+		"full_text": "@whobekindto you might enjoy this https://t.co/m3Uvz1fS5o",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "whobekindto",
+					"name": "Michael Klapdor",
+					"id": 380335289,
+					"id_str": "380335289",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/m3Uvz1fS5o",
+					"expanded_url": "https://twitter.com/garytaphouse/status/1532643161230696448",
+					"display_url": "twitter.com/garytaphouse/s…",
+					"indices": [
+						34,
+						57
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 380335289,
+		"in_reply_to_user_id_str": "380335289",
+		"in_reply_to_screen_name": "whobekindto",
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Adelaide, South Australia",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology.",
+			"url": "https://t.co/uC0c3me6KE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uC0c3me6KE",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 823,
+			"friends_count": 163,
+			"listed_count": 87,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 10079,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15454,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1532643161230696400,
+		"quoted_status_id_str": "1532643161230696448",
+		"quoted_status_permalink": {
+			"url": "https://t.co/m3Uvz1fS5o",
+			"expanded": "https://twitter.com/garytaphouse/status/1532643161230696448",
+			"display": "twitter.com/garytaphouse/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 03 08:39:39 +0000 2022",
+			"id": 1532643161230696400,
+			"id_str": "1532643161230696448",
+			"full_text": "Roberto Carlos vs France, 25 years ago today https://t.co/qgEpQax0Fa",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				44
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1532643126778658800,
+						"id_str": "1532643126778658816",
+						"indices": [
+							45,
+							68
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1532643126778658816/pu/img/xNjkeu9NyeFDlJEI.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1532643126778658816/pu/img/xNjkeu9NyeFDlJEI.jpg",
+						"url": "https://t.co/qgEpQax0Fa",
+						"display_url": "pic.twitter.com/qgEpQax0Fa",
+						"expanded_url": "https://twitter.com/garytaphouse/status/1532643161230696448/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1532643126778658800,
+						"id_str": "1532643126778658816",
+						"indices": [
+							45,
+							68
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1532643126778658816/pu/img/xNjkeu9NyeFDlJEI.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1532643126778658816/pu/img/xNjkeu9NyeFDlJEI.jpg",
+						"url": "https://t.co/qgEpQax0Fa",
+						"display_url": "pic.twitter.com/qgEpQax0Fa",
+						"expanded_url": "https://twitter.com/garytaphouse/status/1532643161230696448/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 67729,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1532643126778658816/pu/pl/NAxgpVdmFuws8gh9.m3u8?tag=12&container=fmp4"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1532643126778658816/pu/vid/640x360/knkCaBZ-L4Q8G51U.mp4?tag=12"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1532643126778658816/pu/vid/480x270/aSAJu2H6Plafy9rQ.mp4?tag=12"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2322400503,
+				"id_str": "2322400503",
+				"name": "Gary Taphouse",
+				"screen_name": "garytaphouse",
+				"location": "Bournemouth ",
+				"description": "Football commentator for @SkySports & PLP since 2005◾Also for IMG, Gravity Media & Pitch Int◾@ucfbuk lecturer◾Poster of sport media jobs since 2015◾Views my own",
+				"url": "https://t.co/qhT0OjIW7v",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/qhT0OjIW7v",
+								"expanded_url": "https://www.instagram.com/garytaphouse/",
+								"display_url": "instagram.com/garytaphouse/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 29308,
+				"friends_count": 3354,
+				"listed_count": 149,
+				"created_at": "Mon Feb 03 18:53:38 +0000 2014",
+				"favourites_count": 18316,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 24483,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1446043378572013571/D0nKQnQ7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1446043378572013571/D0nKQnQ7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2322400503/1621872377",
+				"profile_link_color": "000CB3",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 15200,
+			"favorite_count": 72710,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@whobekindto you might enjoy this https://t.co/m3Uvz1fS5o"
+	},
+	{
 		"created_at": "Fri Jun 03 23:08:20 +0000 2022",
 		"id": 1532861773866729500,
 		"id_str": "1532861773866729472",
@@ -63,12 +388,12 @@ Grailbird.data.tweets_2022_06 =
 			"friends_count": 163,
 			"listed_count": 87,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 10074,
+			"favourites_count": 10079,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 15453,
+			"statuses_count": 15454,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -309,16 +634,16 @@ Grailbird.data.tweets_2022_06 =
 					}
 				},
 				"protected": false,
-				"followers_count": 4377,
-				"friends_count": 2683,
+				"followers_count": 4382,
+				"friends_count": 2680,
 				"listed_count": 8,
 				"created_at": "Sat Oct 19 01:26:54 +0000 2019",
-				"favourites_count": 35537,
+				"favourites_count": 35549,
 				"utc_offset": null,
 				"time_zone": null,
 				"geo_enabled": false,
 				"verified": false,
-				"statuses_count": 24162,
+				"statuses_count": 24164,
 				"lang": null,
 				"contributors_enabled": false,
 				"is_translator": false,
@@ -349,15 +674,15 @@ Grailbird.data.tweets_2022_06 =
 			"place": null,
 			"contributors": null,
 			"is_quote_status": false,
-			"retweet_count": 62,
-			"favorite_count": 110,
+			"retweet_count": 166,
+			"favorite_count": 274,
 			"favorited": false,
 			"retweeted": false,
 			"possibly_sensitive": false,
 			"lang": "en"
 		},
-		"retweet_count": 0,
-		"favorite_count": 1,
+		"retweet_count": 1,
+		"favorite_count": 2,
 		"favorited": false,
 		"retweeted": false,
 		"possibly_sensitive": false,
@@ -413,16 +738,16 @@ Grailbird.data.tweets_2022_06 =
 				}
 			},
 			"protected": false,
-			"followers_count": 22455,
+			"followers_count": 22453,
 			"friends_count": 1966,
 			"listed_count": 345,
 			"created_at": "Sun Mar 08 09:55:16 +0000 2009",
-			"favourites_count": 15653,
+			"favourites_count": 15655,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 37316,
+			"statuses_count": 37318,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -454,7 +779,7 @@ Grailbird.data.tweets_2022_06 =
 		"contributors": null,
 		"is_quote_status": false,
 		"retweet_count": 49,
-		"favorite_count": 94,
+		"favorite_count": 95,
 		"favorited": false,
 		"retweeted": true,
 		"lang": "en",
