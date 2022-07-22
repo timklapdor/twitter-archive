664625f932ce13b708b221f8f1e1cf5d3ca6bc80
Tim Klapdor <tim.klapdor@gmail.com>
2022-07-22 09:47:22 +0930

    Added 3138 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13085,
-	"created_at": "Thu Jul 21 00:19:41 +0000 2022",
+	"tweets": 14057,
+	"created_at": "Fri Jul 22 00:17:20 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_07.js",
 		"year": 2022,
 		"var_name": "tweets_2022_07",
-		"tweet_count": 9,
+		"tweet_count": 10,
 		"month": 7
 	},
 	{
@@ -269,8 +269,106 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_05.js",
 		"year": 2019,
 		"var_name": "tweets_2019_05",
-		"tweet_count": 56,
+		"tweet_count": 61,
 		"month": 5
+	},
+	{
+		"file_name": "data/js/tweets/2019_04.js",
+		"year": 2019,
+		"var_name": "tweets_2019_04",
+		"tweet_count": 47,
+		"month": 4
+	},
+	{
+		"file_name": "data/js/tweets/2019_03.js",
+		"year": 2019,
+		"var_name": "tweets_2019_03",
+		"tweet_count": 69,
+		"month": 3
+	},
+	{
+		"file_name": "data/js/tweets/2019_02.js",
+		"year": 2019,
+		"var_name": "tweets_2019_02",
+		"tweet_count": 72,
+		"month": 2
+	},
+	{
+		"file_name": "data/js/tweets/2019_01.js",
+		"year": 2019,
+		"var_name": "tweets_2019_01",
+		"tweet_count": 82,
+		"month": 1
+	},
+	{
+		"file_name": "data/js/tweets/2018_12.js",
+		"year": 2018,
+		"var_name": "tweets_2018_12",
+		"tweet_count": 61,
+		"month": 12
+	},
+	{
+		"file_name": "data/js/tweets/2018_11.js",
+		"year": 2018,
+		"var_name": "tweets_2018_11",
+		"tweet_count": 51,
+		"month": 11
+	},
+	{
+		"file_name": "data/js/tweets/2018_10.js",
+		"year": 2018,
+		"var_name": "tweets_2018_10",
+		"tweet_count": 41,
+		"month": 10
+	},
+	{
+		"file_name": "data/js/tweets/2018_09.js",
+		"year": 2018,
+		"var_name": "tweets_2018_09",
+		"tweet_count": 58,
+		"month": 9
+	},
+	{
+		"file_name": "data/js/tweets/2018_08.js",
+		"year": 2018,
+		"var_name": "tweets_2018_08",
+		"tweet_count": 86,
+		"month": 8
+	},
+	{
+		"file_name": "data/js/tweets/2018_07.js",
+		"year": 2018,
+		"var_name": "tweets_2018_07",
+		"tweet_count": 91,
+		"month": 7
+	},
+	{
+		"file_name": "data/js/tweets/2018_06.js",
+		"year": 2018,
+		"var_name": "tweets_2018_06",
+		"tweet_count": 56,
+		"month": 6
+	},
+	{
+		"file_name": "data/js/tweets/2018_05.js",
+		"year": 2018,
+		"var_name": "tweets_2018_05",
+		"tweet_count": 96,
+		"month": 5
+	},
+	{
+		"file_name": "data/js/tweets/2018_04.js",
+		"year": 2018,
+		"var_name": "tweets_2018_04",
+		"tweet_count": 109,
+		"month": 4
+	},
+	{
+		"file_name": "data/js/tweets/2018_03.js",
+		"year": 2018,
+		"var_name": "tweets_2018_03",
+		"tweet_count": 47,
+		"month": 3
 	},
 	{
 		"file_name": "data/js/tweets/2016_08.js",
