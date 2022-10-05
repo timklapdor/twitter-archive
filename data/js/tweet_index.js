39b402eaed5a6e1c21b3422163f8b28078180743
Tim Klapdor <tim.klapdor@gmail.com>
2022-10-05 10:47:19 +1030

    Added 3133 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -38,7 +38,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_04.js",
 		"year": 2022,
 		"var_name": "tweets_2022_04",
-		"tweet_count": 47,
+		"tweet_count": 48,
 		"month": 4
 	},
 	{
@@ -304,7 +304,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_02.js",
 		"year": 2019,
 		"var_name": "tweets_2019_02",
-		"tweet_count": 71,
+		"tweet_count": 70,
 		"month": 2
 	},
 	{
