b69f29a74d311e655c146c8a873e1a9588399f07
Tim Klapdor <tim.klapdor@gmail.com>
2022-12-03 10:47:09 +1030

    Added 1360 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14023,
-	"created_at": "Fri Dec 02 00:17:29 +0000 2022",
+	"tweets": 12279,
+	"created_at": "Sat Dec 03 00:17:08 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,395 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Fri Dec 02 09:23:25 +0000 2022",
+		"id": 1598608766089728000,
+		"id_str": "1598608766089728010",
+		"full_text": "@gamerlearner Needs a team to work through this effectively. LD needs to inform the UX and vice-versa.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gamerlearner",
+					"name": "Colin Simpson",
+					"id": 239649193,
+					"id_str": "239649193",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1598607511393046500,
+		"in_reply_to_status_id_str": "1598607511393046528",
+		"in_reply_to_user_id": 174124175,
+		"in_reply_to_user_id_str": "174124175",
+		"in_reply_to_screen_name": "timklapdor",
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Adelaide, South Australia",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology. Also find me @timklapdor@mastodon.social",
+			"url": "https://t.co/83ULNLDqhO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/83ULNLDqhO",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 812,
+			"friends_count": 168,
+			"listed_count": 84,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 10423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15499,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@gamerlearner Needs a team to work through this effectively. LD needs to inform the UX and vice-versa."
+	},
+	{
+		"created_at": "Fri Dec 02 09:22:14 +0000 2022",
+		"id": 1598513757805858800,
+		"id_str": "1598608471062347776",
+		"full_text": "I’m sorry, I simply cannot be cynical about a technology that can accomplish this. https://t.co/yjlY72eZ0m",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1598513753028452400,
+					"id_str": "1598513753028452357",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi8PygHWYAUV8jU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi8PygHWYAUV8jU.jpg",
+					"url": "https://t.co/yjlY72eZ0m",
+					"display_url": "pic.twitter.com/yjlY72eZ0m",
+					"expanded_url": "https://twitter.com/tqbf/status/1598513757805858820/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1656,
+							"h": 1402,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1016,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1598513753028452400,
+					"id_str": "1598513753028452357",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/Fi8PygHWYAUV8jU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fi8PygHWYAUV8jU.jpg",
+					"url": "https://t.co/yjlY72eZ0m",
+					"display_url": "pic.twitter.com/yjlY72eZ0m",
+					"expanded_url": "https://twitter.com/tqbf/status/1598513757805858820/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1656,
+							"h": 1402,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1016,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9395312,
+			"id_str": "9395312",
+			"name": "Thomas H. Ptacek",
+			"screen_name": "tqbf",
+			"location": "✶ ✶ ✶ ✶",
+			"description": "Full of passionate intensity. @tqbf@infosec.exchange",
+			"url": "https://t.co/ytdvKC91VN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ytdvKC91VN",
+							"expanded_url": "http://sockpuppet.org/me",
+							"display_url": "sockpuppet.org/me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30853,
+			"friends_count": 658,
+			"listed_count": 1001,
+			"created_at": "Fri Oct 12 03:32:18 +0000 2007",
+			"favourites_count": 5168,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 91924,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1440053758084272132/dF6N7UBk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1440053758084272132/dF6N7UBk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9395312/1412959140",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8707,
+		"favorite_count": 61215,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m sorry, I simply cannot be cynical about a technology that can accomplish this. https://t.co/yjlY72eZ0m"
+	},
+	{
+		"created_at": "Fri Dec 02 09:18:26 +0000 2022",
+		"id": 1598607511393046500,
+		"id_str": "1598607511393046528",
+		"full_text": "@gamerlearner Having personally reskinned Blackboard and done web design - yes there is a significant difference. One aspect is context of designing for learning - but the more significant one is the tech stack and implementation. So many dependencies that are unfamiliar to a “normal” website.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			294
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gamerlearner",
+					"name": "Colin Simpson",
+					"id": 239649193,
+					"id_str": "239649193",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1598531374818152400,
+		"in_reply_to_status_id_str": "1598531374818152448",
+		"in_reply_to_user_id": 239649193,
+		"in_reply_to_user_id_str": "239649193",
+		"in_reply_to_screen_name": "gamerlearner",
+		"user": {
+			"id": 174124175,
+			"id_str": "174124175",
+			"name": "Tim Klapdor",
+			"screen_name": "timklapdor",
+			"location": "Adelaide, South Australia",
+			"description": "Passionate about good design, motivated by the power of media and enchanted by the opportunities of technology. Also find me @timklapdor@mastodon.social",
+			"url": "https://t.co/83ULNLDqhO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/83ULNLDqhO",
+							"expanded_url": "http://timklapdor.com",
+							"display_url": "timklapdor.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 812,
+			"friends_count": 168,
+			"listed_count": 84,
+			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
+			"favourites_count": 10423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15499,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210875941498437632/O2G3GSW6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/174124175/1398743038",
+			"profile_link_color": "5FA832",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@gamerlearner Having personally reskinned Blackboard and done web design - yes there is a significant difference. One aspect is context of designing for learning - but the more significant one is the tech stack and implementation. So many dependencies that are unfamiliar to a “normal” website."
+	},
+	{
 		"created_at": "Thu Dec 01 05:41:21 +0000 2022",
 		"id": 1598190495033086000,
 		"id_str": "1598190495033085952",
@@ -63,12 +452,12 @@ Grailbird.data.tweets_2022_12 =
 			"friends_count": 168,
 			"listed_count": 84,
 			"created_at": "Tue Aug 03 04:32:42 +0000 2010",
-			"favourites_count": 10420,
+			"favourites_count": 10423,
 			"utc_offset": null,
 			"time_zone": null,
 			"geo_enabled": true,
 			"verified": false,
-			"statuses_count": 15497,
+			"statuses_count": 15499,
 			"lang": null,
 			"contributors_enabled": false,
 			"is_translator": false,
@@ -261,16 +650,16 @@ Grailbird.data.tweets_2022_12 =
 					}
 				},
 				"protected": false,
-				"followers_count": 1330,
-				"friends_count": 1992,
+				"followers_count": 1337,
+				"friends_count": 1993,
 				"listed_count": 11,
 				"created_at": "Thu May 10 11:54:14 +0000 2012",
-				"favourites_count": 50963,
+				"favourites_count": 50985,
 				"utc_offset": null,
 				"time_zone": null,
 				"geo_enabled": true,
 				"verified": false,
-				"statuses_count": 10079,
+				"statuses_count": 10083,
 				"lang": null,
 				"contributors_enabled": false,
 				"is_translator": false,
@@ -301,15 +690,15 @@ Grailbird.data.tweets_2022_12 =
 			"place": null,
 			"contributors": null,
 			"is_quote_status": false,
-			"retweet_count": 479,
-			"favorite_count": 4811,
+			"retweet_count": 500,
+			"favorite_count": 4992,
 			"favorited": false,
 			"retweeted": false,
 			"possibly_sensitive": false,
 			"lang": "en"
 		},
 		"retweet_count": 0,
-		"favorite_count": 3,
+		"favorite_count": 4,
 		"favorited": false,
 		"retweeted": false,
 		"possibly_sensitive": false,
