e11a4e494233662850682d3af313f60f0b91c1cb
Tim Klapdor <tim.klapdor@gmail.com>
2023-02-25 10:47:28 +1030

    Added 2904 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14016,
-	"created_at": "Fri Feb 24 00:17:26 +0000 2023",
+	"tweets": 13823,
+	"created_at": "Sat Feb 25 00:17:27 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -311,7 +311,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_06.js",
 		"year": 2019,
 		"var_name": "tweets_2019_06",
-		"tweet_count": 48,
+		"tweet_count": 47,
 		"month": 6
 	},
 	{
@@ -353,7 +353,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 54,
+		"tweet_count": 53,
 		"month": 12
 	},
 	{
@@ -395,22 +395,8 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_06.js",
 		"year": 2018,
 		"var_name": "tweets_2018_06",
-		"tweet_count": 54,
+		"tweet_count": 2,
 		"month": 6
-	},
-	{
-		"file_name": "data/js/tweets/2018_05.js",
-		"year": 2018,
-		"var_name": "tweets_2018_05",
-		"tweet_count": 93,
-		"month": 5
-	},
-	{
-		"file_name": "data/js/tweets/2018_04.js",
-		"year": 2018,
-		"var_name": "tweets_2018_04",
-		"tweet_count": 46,
-		"month": 4
 	},
 	{
 		"file_name": "data/js/tweets/2016_08.js",
