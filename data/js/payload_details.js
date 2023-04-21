cf59c4ccffb85cf604cd2a9ffaec01871229e045
Tim Klapdor <tim.klapdor@gmail.com>
2023-04-21 09:47:27 +0930

    Added 3108 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14030,
-	"created_at": "Thu Apr 20 00:19:58 +0000 2023",
+	"tweets": 14027,
+	"created_at": "Fri Apr 21 00:17:25 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2023_04.js",
 		"year": 2023,
 		"var_name": "tweets_2023_04",
-		"tweet_count": 1,
+		"tweet_count": 2,
 		"month": 4
 	},
 	{
@@ -87,7 +87,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_04.js",
 		"year": 2022,
 		"var_name": "tweets_2022_04",
-		"tweet_count": 46,
+		"tweet_count": 47,
 		"month": 4
 	},
 	{
@@ -122,7 +122,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_11.js",
 		"year": 2021,
 		"var_name": "tweets_2021_11",
-		"tweet_count": 43,
+		"tweet_count": 42,
 		"month": 11
 	},
 	{
@@ -199,7 +199,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_12.js",
 		"year": 2020,
 		"var_name": "tweets_2020_12",
-		"tweet_count": 59,
+		"tweet_count": 58,
 		"month": 12
 	},
 	{
@@ -276,7 +276,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_01.js",
 		"year": 2020,
 		"var_name": "tweets_2020_01",
-		"tweet_count": 118,
+		"tweet_count": 117,
 		"month": 1
 	},
 	{
@@ -423,7 +423,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_04.js",
 		"year": 2018,
 		"var_name": "tweets_2018_04",
-		"tweet_count": 56,
+		"tweet_count": 54,
 		"month": 4
 	},
 	{
