0970cd336b6ce211b20414329f416b4d325f6cc3
Tim Klapdor <tim.klapdor@gmail.com>
2023-04-24 09:47:19 +0930

    Added 2338 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14027,
-	"created_at": "Sun Apr 23 00:17:27 +0000 2023",
+	"tweets": 13257,
+	"created_at": "Mon Apr 24 00:17:18 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -346,85 +346,8 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_03.js",
 		"year": 2019,
 		"var_name": "tweets_2019_03",
-		"tweet_count": 66,
+		"tweet_count": 21,
 		"month": 3
-	},
-	{
-		"file_name": "data/js/tweets/2019_02.js",
-		"year": 2019,
-		"var_name": "tweets_2019_02",
-		"tweet_count": 71,
-		"month": 2
-	},
-	{
-		"file_name": "data/js/tweets/2019_01.js",
-		"year": 2019,
-		"var_name": "tweets_2019_01",
-		"tweet_count": 80,
-		"month": 1
-	},
-	{
-		"file_name": "data/js/tweets/2018_12.js",
-		"year": 2018,
-		"var_name": "tweets_2018_12",
-		"tweet_count": 54,
-		"month": 12
-	},
-	{
-		"file_name": "data/js/tweets/2018_11.js",
-		"year": 2018,
-		"var_name": "tweets_2018_11",
-		"tweet_count": 48,
-		"month": 11
-	},
-	{
-		"file_name": "data/js/tweets/2018_10.js",
-		"year": 2018,
-		"var_name": "tweets_2018_10",
-		"tweet_count": 42,
-		"month": 10
-	},
-	{
-		"file_name": "data/js/tweets/2018_09.js",
-		"year": 2018,
-		"var_name": "tweets_2018_09",
-		"tweet_count": 54,
-		"month": 9
-	},
-	{
-		"file_name": "data/js/tweets/2018_08.js",
-		"year": 2018,
-		"var_name": "tweets_2018_08",
-		"tweet_count": 84,
-		"month": 8
-	},
-	{
-		"file_name": "data/js/tweets/2018_07.js",
-		"year": 2018,
-		"var_name": "tweets_2018_07",
-		"tweet_count": 91,
-		"month": 7
-	},
-	{
-		"file_name": "data/js/tweets/2018_06.js",
-		"year": 2018,
-		"var_name": "tweets_2018_06",
-		"tweet_count": 54,
-		"month": 6
-	},
-	{
-		"file_name": "data/js/tweets/2018_05.js",
-		"year": 2018,
-		"var_name": "tweets_2018_05",
-		"tweet_count": 93,
-		"month": 5
-	},
-	{
-		"file_name": "data/js/tweets/2018_04.js",
-		"year": 2018,
-		"var_name": "tweets_2018_04",
-		"tweet_count": 54,
-		"month": 4
 	},
 	{
 		"file_name": "data/js/tweets/2016_08.js",
